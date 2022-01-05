19b9ef1d42e5e199330e3761203edabc01b7fcc3
SnehalKaranje <snehalkaranje9@gmail.com>
2022-01-05 21:16:57 +0530

    list of heroes with mock data

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero [] = [
+  {id : 1, name : "A.P.J. Abdul Kalam"},
+  {id : 2, name : "Lokmanya Bal Gangadhar Tilak"},
+  {id : 3, name : "Swami Vivekananda"},
+  {id : 4, name : "Vinayak Damodar Savarkar"},
+  {id : 5, name : "Anandi Gopal"},
+  {id : 6, name : "Savitribai Phule"},
+  {id : 7, name : "Sheryl Sandberg"}
+];
