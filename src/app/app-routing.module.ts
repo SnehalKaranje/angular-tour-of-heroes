cbbfe9f9adc41d1beb7e5f90d60538cccc6705f8
SnehalKaranje <snehalkaranje9@gmail.com>
2022-01-06 18:51:26 +0530

    Routing initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes : Routes = [{ 
+  path: 'heroes', 
+  component: HeroesComponent 
+},{ 
+  path: '', 
+  redirectTo: '/dashboard', 
+  pathMatch: 'full' 
+},{
+  path: 'dashboard',
+  component : DashboardComponent
+}];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
